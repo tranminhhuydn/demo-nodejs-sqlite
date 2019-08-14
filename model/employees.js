f6151d67649770330a52630e6b1e36c499cacc84
tranminhhuydn <tranminhhuydn@gmail.com>
2019-08-14 17:47:28 +0700

    demo nodejs sqlite

---
+++ model/employees.js
@@ -0,0 +1,14 @@
+var Sequelize = require('sequelize');
+var sequelize = module.parent.exports.sequelize;
+
+var Employee = sequelize.define('Employees', {
+	idEmployee      : { type : Sequelize.INTEGER, primaryKey : true, autoIncrement : true },
+	first_name      : Sequelize.TEXT,
+	last_name       : Sequelize.TEXT,
+	email           : Sequelize.TEXT,
+	hashed_password : Sequelize.TEXT
+}, {
+	timestamps: false
+});
+
+module.exports = Employee;
---
+++ routes/users.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var router = express.Router();
+//var userDAO = require('../dao/userDAO');
+var result = require('../model/result');
+
+var sequelize = exports.sequelize = require('./conn.js');
+var Employee = require('./model/employees.js');
+
+
+
+/* list users */
+router.get('/', function(req, res) {
+    console.log('list users called');
+//    userDAO.list(function (users) {
+//        res.json(result.createResult(true, users));
+//    });
+	  Employee.findAll().then(function(_employee){
+	    res.json(result.createResult(true, _employee));
+	  });
+
+});
+
+///* get user */
+//router.get('/:id', function(req, res) {
+//    var id = req.params.id;
+//    console.log('get user called, id: ' + id);
+//    userDAO.getById(id, function (user) {
+//        res.json(result.createResult(true, user));
+//    });
+//});
+//
+///* delete user */
+//router.delete('/:id', function (req, res) {
+//    var id = req.params.id;
+//    console.log('delete user called, id=' + id);
+//    userDAO.deleteById(id, function (success) {
+//        res.json(result.createResult(success, null));
+//    });
+//});
+//
+///* add users */
+//router.post('/', function (req, res) {
+//    console.log('post users called');
+//    var user = req.body;
+//    console.log(user);
+//    userDAO.add(user, function (success) {
+//        var r =  result.createResult(success, null);
+//        res.json(r);
+//    });
+//});
+//
+///* update users */
+//router.put('/:id', function (req, res) {
+//    console.log('update users called');
+//    var user = req.body;
+//    user.id = req.params.id;
+//    console.log(user);
+//    userDAO.update(user, function (success) {
+//        var r =  result.createResult(success, null);
+//        res.json(r);
+//    });
+//});
+//
+///* patch users */
+//router.patch('/:id', function (req, res) {
+//    console.log('patch users called');
+//    userDAO.getById(req.params.id, function (user) {
+//        var username = req.body.username;
+//        if(username) {
+//            user.username = username;
+//        }
+//        var password = req.body.password;
+//        if(password) {
+//            user.password = password;
+//        }
+//        console.log(user);
+//        userDAO.update(user, function (success) {
+//            var r =  result.createResult(success, null);
+//            res.json(r);
+//        });
+//    });
+//});
+
+module.exports = router;
