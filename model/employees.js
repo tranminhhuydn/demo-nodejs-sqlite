ae1abc5a93662b168e908d36603df16ce207a2a9
tranminhhuydn <tranminhhuydn@gmail.com>
2019-08-15 00:26:14 +0700

    777

--- model/employees.js
+++ model/employees.js
@@ -14,6 +14,7 @@ var Employee = sequelize.define('Employees', {
 }, {
 	timestamps: false
 });
+module.exports = Employee;
 	
 router.get('/employees', function(req, res){
   Employee.findAll().then(function(result){
@@ -22,4 +23,4 @@ router.get('/employees', function(req, res){
   });
 });
 module.exports = router;
-//module.exports = Employee;
+
